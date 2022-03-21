7c569114c1b579ecfc5eb76eef779a61206727c5
Billuboy <72341331+Billuboy@users.noreply.github.com>
2022-03-21 23:55:41 +0530

    Linting Error resolved

--- src/components/registerModal.jsx
+++ src/components/registerModal.jsx
@@ -12,7 +12,7 @@ export default function LoginModal() {
   const [name, setName] = useState('');
   let timer;
 
-  useEffect(() => () => clearTimeout(timer), []);
+  useEffect(() => () => clearTimeout(timer), [timer]);
 
   function closeModal() {
     setIsOpen(false);
